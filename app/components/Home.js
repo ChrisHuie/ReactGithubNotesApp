d3adc65cd411561f2b0776e0392a8e092e5f577f
Chris <cahuie@hotmail.com>
2016-07-06 21:38:07 -0700

    initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Geek on 7/6/16.
+ */
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function () {
+        return (
+            <h2 className="text-center">
+                Search by Github Username Above
+            </h2>
+        )
+    }
+});
