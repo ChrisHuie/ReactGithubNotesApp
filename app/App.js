d7307ba511fa00b63f4670178ec0899da5c74574
Chris <cahuie@hotmail.com>
2016-07-14 21:44:27 -0700

    fixed webpack config bug from not supporting the jsx loader

--- app/App.js
+++ app/App.js
@@ -3,11 +3,12 @@
  */
 
 var React = require('react');
-var ReactDOM = require('react-dom');
+var ReactDom = require('react-dom');
 var Router = require('react-router').Router;
 var routes = require('./config/routes');
 
-ReactDOM.render(
+
+ReactDom.render(
     <Router>{routes}</Router>,
     document.getElementById('app')
-);
+)
